800c9d42384ae8c8a8dad96a0afc81cc2c75584d
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-02-27 17:29:14 +0200

    add api

---
+++ src/api/api.js
@@ -0,0 +1,35 @@
+const BASE_URL = 'http://api.themoviedb.org/3';
+const KEY = '6e66186e83ecfb22dd565ba7420dfbd3';
+const LANGUAGE = 'en-US';
+
+export const getTrendingMovies = () => {
+  return fetch(
+    `${BASE_URL}/trending/all/day?api_key=${KEY}`.then(data => data.json())
+  );
+};
+
+export const getMovie = query => {
+  return fetch(
+    `${BASE_URL}/search/movie/?api_key=${KEY}&query=${query}&language=${LANGUAGE}&page=1`.then(
+      data => data.json()
+    )
+  );
+};
+
+export const getFullInfo = id => {
+  return fetch(
+    `${BASE_URL}/movie/${id}?api_key=${KEY}&language=${LANGUAGE}`
+  ).then(data => data.json);
+};
+
+export const getCast = id => {
+  return fetch(
+    `${BASE_URL}/movie/${id}/credits?api_key=${KEY}&language=${LANGUAGE}`
+  ).then(data => data.json());
+};
+
+export const getReviews = id => {
+  return fetch(
+    `${BASE_URL}/movie/${id}/reviews?api_key=${KEY}&language=${LANGUAGE}&page=1`
+  );
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import {
+  getTrendingMovies,
+  getMovie,
+  getFullInfo,
+  getCast,
+  getReviews,
+} from '../api/api';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  console.log();
 };
